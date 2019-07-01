2d304553bec722e65a4f1f2fff7495b1a902f3fc
josuemnez <josue@josuemartinez.com>
2019-07-01 10:46:12 -0400

    Adding an Intent for URL

--- app/src/main/java/com/josuemartinez/earthquakeapp/Earthquake.java
+++ app/src/main/java/com/josuemartinez/earthquakeapp/Earthquake.java
@@ -12,21 +12,25 @@ public class Earthquake {
     /** Time of the earthquake */
     private long mTimeInMilliseconds;
 
+    /** Website URL of the earthquake */
+    private String mUrl;
+
 
     /**
      * Constructs a new {@link Earthquake} object.
      *
      * @param magnitude is the magnitude (size) of the earthquake
-     * @param location is the city location of the earthquake
+     * @param location is the location where the earthquake happened
      * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
-     *  earthquake happened
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
      */
-    public Earthquake(double magnitude, String location, long timeInMilliseconds) {
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
-
 
     /**
      * Returns the magnitude of the earthquake
@@ -45,4 +49,8 @@ public class Earthquake {
      */
     public long getTimeInMilliseconds() {return mTimeInMilliseconds; }
 
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {return mUrl;}
 }
